73ddae67fd2c0d283713910380c116a0084f2c33
Aurel Ioan Patrutiu <aureldevices@hotmail.com>
2017-02-28 00:03:12 +0100

    Cambio de edificio, introduccion de edificios laterales, venja , etc

--- js/canvas.js
+++ js/canvas.js
@@ -7,7 +7,8 @@ function Canvas(){
 	this.renderer = null;
 	this.camera = null;
 	this.scene = null;
-	this.control = null;	
+	this.control = null;
+	this.textureLoader= null;	
 };
 
 Canvas.prototype.init = function(){
@@ -20,22 +21,27 @@ Canvas.prototype.init = function(){
 	
 	this.createPlane(1200,2,1200);
 	//Metele posicione
+	this.createSceneWalls();
+	//Metele posicione
 	this.createAcera();
 	//Metele posicione
 	this.createStreet();
+	
 	//Metele posicione
-	this.createFrontWall();
+	//this.createBuilding(-560,0,-200);
+
+	this.createOBJ(0,0,-400,"building");
+
 	//Metele posicione
-	this.createBuilding(-560,0,-200);
-	//Metele posicione
-	this.createStop();
+	//this.createStop();
 	this.createLamp(280,0,65);
 	this.createLamp(-280,0,65);
+	this.createSideBuildings(539,78,0);
 	this.createSkybox();
-	this.createDrawPlane(-187,32,2);
-	this.createDrawPlane(187,32,2);
-	this.createDrawPlane(59,32,2);
-	this.createDrawPlane(-69,32,2);
+	this.createDrawPlane(-256,32,2);
+	this.createDrawPlane(-128,32,2);
+	this.createDrawPlane(256,32,2);
+	this.createDrawPlane(128,32,2);
 
 
 	//this.createLight(0,100,200);
@@ -45,7 +51,8 @@ Canvas.prototype.init = function(){
 //Funcion para crear la escena de nuestro mundo 3D
 Canvas.prototype.createScene = function(){
 	this.scene = new THREE.Scene();
-	this.scene.fog = new Fog(0x9c9c9c , 100,1000);
+	this.scene.fog = new Fog(0x9c9c9c , 200,1000);
+	this.textureLoader = new THREE.TextureLoader();
 }
 
 //Funcion para crear el renderer del mundo 3D
@@ -91,30 +98,39 @@ Canvas.prototype.createPlane = function(x,y,z){
 }
 
 Canvas.prototype.createAcera = function(){
-	var geometry = new THREE.CubeGeometry(600,4,70);
+
+	var frontal = new THREE.CubeGeometry(640,2,70);
 	var texture = new THREE.ImageUtils.loadTexture('imgs/baldosa.jpg');
 
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 60, 8);
 
 	var material = new THREE.MeshPhongMaterial({map: texture});
-	var front = new THREE.Mesh(geometry,material);
-	front.position.set(0,2,35);
+	var front = new THREE.Mesh(frontal,material);
+	front.position.set(0,1,35);
 	
-	var texture2 = new THREE.ImageUtils.loadTexture('imgs/baldosa.jpg');
-	texture2.wrapS = texture2.wrapT = THREE.RepeatWrapping;
-	texture2.repeat.set( 8, 60);
-	var material2 = new THREE.MeshPhongMaterial({map: texture2});
+	var lateralTexture = new THREE.ImageUtils.loadTexture('imgs/baldosa.jpg');
+	lateralTexture.wrapS = lateralTexture.wrapT = THREE.RepeatWrapping;
+	lateralTexture.repeat.set( 8, 60);
+	var material2 = new THREE.MeshPhongMaterial({map: lateralTexture});
 	
-	var geometry1 = new THREE.CubeGeometry(50,4,600);
-	var left = new THREE.Mesh(geometry1,material2);
-	left.position.set(-275,2,-300);
+	//interiores
+	var lateral = new THREE.CubeGeometry(70,2,640);
+	var left = new THREE.Mesh(lateral,material2);
+	left.position.set(-355,1,-250);
+	var right = new THREE.Mesh(lateral,material2);
+	right.position.set(355,1,-250);
 
-	var right = new THREE.Mesh(geometry1,material2);
-	right.position.set(275,2,-300);
-
+	//exteriores
+	var lateral2 = new THREE.CubeGeometry(60,2,640);
+	var left2 = new THREE.Mesh(lateral2,material2);
+	left2.position.set(-520,1,-150);
+	var right2 = new THREE.Mesh(lateral2,material2);
+	right2.position.set(520,1,-150);
 	this.scene.add(left);
 	this.scene.add(right);
+	this.scene.add(left2);
+	this.scene.add(right2);
 	this.scene.add(front);
 }
 
@@ -132,6 +148,26 @@ Canvas.prototype.createBuilding = function(x,y,z){
 			that.scene.add(object);			
 		});
 	});
+}
+
+Canvas.prototype.createOBJ = function(x,y,z,name){
+	var that = this;
+	var manager = new THREE.LoadingManager();
+	var texture = this.textureLoader.load('obj/build10/Build10.jpg')
+	var objLoader = new THREE.OBJLoader(manager);
+    var material = new THREE.MeshBasicMaterial({map:texture});
+    objLoader.load('obj/build10/Build10_obj.obj', function (obj) {
+        obj.traverse(function (child) {
+
+            if (child instanceof THREE.Mesh) {
+                child.material = material;
+            }
+            obj.scale.set(1.5,1,1.5);
+            obj.name = name;
+            obj.position.set(x,y,z);
+        });
+        that.scene.add(obj);
+    });
 }
 
 Canvas.prototype.createStop = function() {
@@ -190,7 +226,7 @@ Canvas.prototype.createTree = function(x,y,z) {
 Canvas.prototype.createStreet = function(){
 	
 	//CARRETERA HORIZONTAL
-	var geometry = new THREE.CubeGeometry(600,1,100);
+	var geometry = new THREE.CubeGeometry(780 ,1,100);
 	var texture = new THREE.ImageUtils.loadTexture('imgs/carretera.jpg');
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 5, 1);
@@ -204,11 +240,11 @@ Canvas.prototype.createStreet = function(){
 	var cornerTexture = new THREE.ImageUtils.loadTexture('imgs/esq_drch.jpg');
 	var cornerMaterial = new THREE.MeshPhongMaterial({map: cornerTexture});
 	var rightCorner  = new THREE.Mesh(cornerGeometry,cornerMaterial);
-	rightCorner.position.set(-350,1,120);
+	rightCorner.position.set(-440,1,120);
 	var cornerTextDer = THREE.ImageUtils.loadTexture('imgs/esq_izq.jpg');
 	var cornerMaterial2 = new THREE.MeshPhongMaterial({map: cornerTextDer});
 	var leftCorner  = new THREE.Mesh(cornerGeometry,cornerMaterial2);
-	leftCorner.position.set(350,1,120);
+	leftCorner.position.set(440,1,120);
 
 	//CARETRAS PARALELAS HACIA DENTRO
 	var sideStrets = new THREE.CubeGeometry(100,1,600);
@@ -219,8 +255,8 @@ Canvas.prototype.createStreet = function(){
 	leftStreet = new THREE.Mesh(sideStrets,material2);
 	rightStreet = new THREE.Mesh(sideStrets,material2);
 
-	leftStreet.position.set(350,1,-230);
-	rightStreet.position.set(-350,1,-230);
+	leftStreet.position.set(440,1,-230);
+	rightStreet.position.set(-440,1,-230);
 
 	this.scene.add(leftStreet);
 	this.scene.add(rightStreet);
@@ -229,29 +265,41 @@ Canvas.prototype.createStreet = function(){
 	this.scene.add(plane);
 }
 
-Canvas.prototype.createFrontWall = function(){
-	var geometry = new THREE.CubeGeometry(500,64,3);
+Canvas.prototype.createSceneWalls = function(){
+	var front = new THREE.CubeGeometry(256,64,3);
 	var texture = new THREE.ImageUtils.loadTexture('imgs/wall.3.jpg');
 	
 	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 	texture.repeat.set( 6, 1);
 
 	var material = new THREE.MeshPhongMaterial({map: texture});
-	var wall = new THREE.Mesh(geometry,material);
-	wall.position.set(0,32,0);
-	wall.name = "wall";
-	
+	var frontLeft = new THREE.Mesh(front,material);
+	var frontRight = new THREE.Mesh(front,material);
 
-	var geometry2 = new THREE.CubeGeometry(3,64,500);
-	var rightWall = new THREE.Mesh(geometry2,material);
-	rightWall.position.set(249,32,-249);
+	frontLeft.position.set(-192,32,0);
+	frontRight.position.set(192,32,0);
 
-	var leftWall = new THREE.Mesh(geometry2,material);
-	leftWall.position.set(-249,32,-249);
-	
-	this.scene.add(wall);
+	//Geometria de las parede laterales
+	var lateral = new THREE.CubeGeometry(3,64,640);
+	//pared derecha
+	var rightWall = new THREE.Mesh(lateral,material);
+	rightWall.position.set(319,32,-320);
+
+	var leftWall = new THREE.Mesh(lateral,material);
+	leftWall.position.set(-319,32,-320);
+
+	//Entrance
+	var gateGeo = new THREE.PlaneGeometry(128,130);
+	var gateTexture = this.textureLoader.load('imgs/entrance.png');
+	var gateMat = new THREE.MeshPhongMaterial({map:gateTexture,transparent:true});
+	var gate = new THREE.Mesh(gateGeo,gateMat);
+	gate.position.set(0,40,0);
+
+	this.scene.add(frontLeft);
+	this.scene.add(frontRight);
 	this.scene.add(leftWall);
 	this.scene.add(rightWall);
+	this.scene.add(gate);
 }
 
 Canvas.prototype.createSkybox = function(){
@@ -265,14 +313,12 @@ Canvas.prototype.createSkybox = function(){
 	for (var i = 0; i < 6; i++)
 	   materialArray[i].side = THREE.BackSide;
 	var skyboxMaterial = new THREE.MeshFaceMaterial( materialArray );
-	var skyboxGeom = new THREE.CubeGeometry( 1200, 1000, 1200, 1, 1, 1 );
+	var skyboxGeom = new THREE.CubeGeometry( 1100, 1000, 1100, 1, 1, 1 );
 	var skybox = new THREE.Mesh( skyboxGeom, skyboxMaterial );
+	skybox.position.set(0,0,-50);
 	this.scene.add( skybox );
 }
 
-Canvas.prototype.degToRad = function(degrees) {
-	return degrees * (Math.PI/180);
-};
 
 Canvas.prototype.createDrawPlane = function(x,y,z) {
 	var canvas = document.querySelector('#twodCanvas');
@@ -285,30 +331,26 @@ Canvas.prototype.createDrawPlane = function(x,y,z) {
 	this.scene.add(draw);
 };
 
+Canvas.prototype.createSideBuildings = function(x,y,z) {
+	var geometry = new THREE.PlaneGeometry(1200,200);
+	var textureLoader = new THREE.TextureLoader();
+	var texture = textureLoader.load('imgs/sidebuild.png');
+	
+	texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set( 5, 1);
+	
+	var material = new THREE.MeshPhongMaterial({map: texture, transparent: true});
+	var leftSide = new THREE.Mesh(geometry,material);
+	var rightSide = new THREE.Mesh(geometry,material);
+	leftSide.position.set(-x,y,z);
+	leftSide.rotateY(this.degToRad(90));
+	rightSide.position.set(x,y,z);
+	rightSide.rotateY(this.degToRad(-90));
+	
+	this.scene.add(leftSide);
+	this.scene.add(rightSide);
+};
 
-
-/*Canvas.prototype.loadOBJ = function(objPath,texturePath,scene,x,y,z)
-{
-    var loader = new THREE.OBJLoader();
-    var that = this;
-    loader.load(objPath, function (object) {
-        object.traverse( function ( child ) {
-             if ( child instanceof THREE.Mesh )
-             {
-                 var texture = new THREE.Texture();
-                 var imgLoader = new THREE.ImageLoader();
-
-                 imgLoader.load( texturePath, function ( image ) {
-                     texture.image = image;
-                     texture.needsUpdate = true;
-                 } );
-                 child.material.map = texture;
-                 }
-             } );
-
-        that.mesh = object;
-        scene.add(object);
-        that.move(0, -10, -30);
-    });
-    return;
-};*/
+Canvas.prototype.degToRad = function(degrees) {
+	return degrees * (Math.PI/180);
+};
